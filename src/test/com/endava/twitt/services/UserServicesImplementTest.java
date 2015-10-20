157b4ddede3f831fc5c80ccf73dc4a1e242d8d09
Igor Balanici <igor.balanici@gmail.com>
2015-10-20 17:04:13 +0300

    added userservicesimplementtes. no implementations

---
+++ src/test/com/endava/twitt/services/UserServicesImplementTest.java
@@ -0,0 +1,36 @@
+package com.endava.twitt.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ibalanici on 20/10/2015.
+ */
+public class UserServicesImplementTest {
+
+    @Test
+    public void testInsertUser() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUserByName() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+
+    }
+}
