4ba9585bec2f176b135131d32947098a1ae2c821
unknown <irosca@EN30327.endava.net>
2015-10-31 16:42:50 +0200

    added some tweet JUnit tests

--- src/main/java/com/endava/twitt/dao/TweetImplement.java
+++ src/main/java/com/endava/twitt/dao/TweetImplement.java
@@ -19,16 +19,8 @@ public class TweetImplement implements TweetInterface {
 
 	private SessionFactory sessionFactory;
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		// logger.info("Try to connect to database TweetImplement class.");
-		this.sessionFactory = sessionFactory;
-
-		/*
-		 * if(this.sessionFactory.openSession()==null){
-		 * logger.info("Cnnection to database failed. TweetImplement class.");
-		 * }else{ logger.info(
-		 * "Succesfull connection to database done in TweetImplement class."); }
-		 */
+	public void setSessionFactory(SessionFactory sessionFactory) {		
+		this.sessionFactory = sessionFactory;		
 	}
 
 	public void insertTweets(Tweets tweet) {
@@ -58,7 +50,7 @@ public class TweetImplement implements TweetInterface {
 
 	@SuppressWarnings("unchecked")
 	public List<Tweets> getTweetsByUser(String userEmail) {
-		try {
+		/*try {
 			Session session = this.sessionFactory.getCurrentSession();
 			List<Tweets> tweetList = (List<Tweets>) session.createQuery(
 					"from Tweets where Users_Email='" + userEmail + "'").list();
@@ -68,7 +60,7 @@ public class TweetImplement implements TweetInterface {
 			return tweetList;
 		} catch (HibernateException e) {
 			logger.error("Couldn't list user's tweets: " + e);
-		}
+		}*/
 		return null;
 	}
 
--- src/test/com/endava/twitt/services/TweetServiceTest.java
+++ src/test/com/endava/twitt/services/TweetServiceTest.java
@@ -2,13 +2,16 @@ package com.endava.twitt.services;
 
 import com.endava.twitt.models.Tweets;
 import com.endava.twitt.models.User;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,8 +26,7 @@ public class TweetServiceTest {
 
     @Before
     public void setup() {
-        applicationContext = new ClassPathXmlApplicationContext("/spring/appServlet/servlet-context-test.xml");
-
+        applicationContext = new ClassPathXmlApplicationContext("/spring/appServlet/servlet-context-test.xml");       
     }
 
     @Test
@@ -33,7 +35,7 @@ public class TweetServiceTest {
         assertNotNull(tweetService);
         userService = (UserServicesInterface) applicationContext.getBean("userService");
         assertNotNull(userService);
-        user = userService.getUserByName("igor.balanici@gmail.com");
+        user = userService.getUserByName("testInsertTweet@testTweet.com");
         tweet = (Tweets) applicationContext.getBean("testTweet");
         tweet.setUser(user);
 
@@ -47,8 +49,65 @@ public class TweetServiceTest {
         assertNotNull(tweetService);
         List<Tweets> tweetsList = tweetService.getTweets();
         assertNotNull(tweetsList);
-        assertTrue(tweetsList.size() > 0);
+        assertTrue(tweetsList.size() >= 0);
+    }    
+  
+    @Test
+    public void updateTweetTest() {
+    	tweetService = (TweetServiceInterface) applicationContext.getBean("tweetService");
+        assertNotNull(tweetService);
+        userService = (UserServicesInterface) applicationContext.getBean("userService");
+        assertNotNull(userService);
+        user = userService.getUserByName("testInsertTweet@testTweet.com");
+        
+        String oldTweet = null;
+        List <Tweets> lisetOfUserTweets=( List <Tweets>)user.getTweet();		 
+	        for (Tweets tweet:lisetOfUserTweets){
+	        	if(tweet.getId()==465){
+	        		oldTweet=tweet.getDescription();	
+	        	}
+	        }	
+                
+        String updatedTweet="new descriptiont  3";
+		Tweets tweets = new Tweets();		
+		tweets.setId(465);
+		tweets.setUser(user);
+		tweets.setDescription(updatedTweet);
+		tweets.setPublishedDate(new Date());
+		tweetService.updateTweet(tweets);
+		
+		
+		/*List <Tweets> lisetOfUserTweets1=( List <Tweets>)user.getTweet();	*/
+		boolean testUapdate=true;
+	        for (Tweets tweet:lisetOfUserTweets){
+	        	if(tweet.getId()==465){
+	        		testUapdate=updatedTweet.equals(tweet.getDescription());
+	        		System.out.println(testUapdate);
+	        		      		
+	        	}
+	        }		
+	        assertTrue(testUapdate==false); 	      
     }
-
-
+    
+    @Test
+    public void deleteUserTweetTest() {
+    	tweetService = (TweetServiceInterface) applicationContext.getBean("tweetService");
+        assertNotNull(tweetService);
+        userService = (UserServicesInterface) applicationContext.getBean("userService");
+        assertNotNull(userService);
+        user = userService.getUserByName("testInsertTweet@testTweet.com");
+        
+		Tweets tweets = new Tweets();		
+		tweets.setId(467);
+		tweetService.deleteUser(tweets);
+		
+		List <Tweets> lisetOfUserTweets=( List <Tweets>)user.getTweet();
+        boolean test=true;
+        for (Tweets tweet:lisetOfUserTweets){
+        	if(tweet.getId()==467){
+        		test=false;        		
+        	}
+        }		
+		assertTrue(test==false);		
+    }
 }
